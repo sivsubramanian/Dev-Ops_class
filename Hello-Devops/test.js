48370f93b1e4fcd9603136810062006a9f1b8b4a
sivasubramanian <sivasufriend@gmail.com>
2025-05-15 11:21:41 +0530

    ""

---
+++ Hello-Devops/test.js
@@ -0,0 +1,9 @@
+const greet = require('./app');
+
+if(greet()=== "Hello from DevOps!") {
+    console.log("Test passed!");
+    Process.exit(0);
+} else {
+    console.log("Test failed!");
+    Process.exit(1);
+}
