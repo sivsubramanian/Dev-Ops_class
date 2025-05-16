dbc6c16038e5fc4fb524873626e22aff157901d2
sivasubramanian <sivasufriend@gmail.com>
2025-05-16 09:10:30 +0530

    updated and proper output

--- Hello-Devops/test.js
+++ Hello-Devops/test.js
@@ -1,9 +1,9 @@
 const greet = require('./app');
 
 if (greet() === "Hello from DevOps!") {
-    console.log("Test passed!");
+    console.log("✅Test passed!");
     process.exit(0); 
 } else {
-    console.log("Test failed!");
+    console.log("❌Test failed!");
     process.exit(1); 
 }
